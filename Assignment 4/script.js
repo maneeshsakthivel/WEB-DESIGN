078c522e1b18bb4915412a82858123bd8df060c2
Maneesh <maneesh12325@gmail.com>
2021-02-17 22:23:36 -0500

    Added validations for checkboxes and result page displayed

--- Assignment 4/script.js
+++ Assignment 4/script.js
@@ -1,8 +1,15 @@
 
 var checkboxes = document.getElementsByClassName("howHear");
+var selectCheckboxOptions = ['Large', 'Small', 'Extra Small', 'Extra Large', 'Medium'];
 
 function finalCheck(){
-    return isChecked();
+    var isChked = isChecked();
+    if (isChked){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 function validateFields(obj,errorDivId,type){
@@ -51,23 +58,109 @@ function validateFields(obj,errorDivId,type){
 }
 
 function showErrorMsg(divError, textField){
-    textField.style.border = "2px solid red";
-    textField.value = "";
+    if (textField){
+        textField.style.border = "2px solid red";
+        textField.value = "";
+    }
     divError.style.display = "block";
-    divError.style.border = ""
+    divError.style.border = "";
     divError.style.color = "red";
 }
 
 function clearErrorMsg(divError, textField){
     divError.style.display = "none";
     divError.style.color = "red";
-    textField.style.border = ""
+    if (textField){
+        textField.style.border = "";
+    }
 }
 
 function isChecked(){
-    for (ele in checkboxes){
-        if (ele.isChecked) return true;
+    var errorDiv = document.getElementById("errorSource");
+    for (ele of checkboxes){
+        if (ele.checked){
+            clearErrorMsg(errorDiv, null);
+            return true;
+        } 
     }
+    showErrorMsg(errorDiv, null);
     return false;
 }
 
+function drinkslistChange(obj){
+    
+    if (document.getElementsByClassName("chkbox").length >= 1){
+        var changingLabel = document.getElementById("labelTochange");
+        var chkboxChange = document.getElementById("chkboxChange");
+        changingLabel.innerHTML  = selectCheckboxOptions[obj.selectedIndex];
+        chkboxChange.setAttribute("value", selectCheckboxOptions[obj.selectedIndex]);
+
+    }
+    else{
+        var formDiv = document.getElementById("form_elements");
+        console.log(obj);
+        var chkbox = document.createElement('input')
+        chkbox.className = "chkbox"
+        chkbox.id = "chkboxChange"
+        chkbox.setAttribute('type', 'checkbox');
+        chkbox.setAttribute('name', 'CheckboxValue');
+        chkbox.setAttribute("onclick", addTextField());
+        chkbox.addEventListener( 'change', function() {
+            if(this.checked) {
+                // Checkbox is checked..
+
+                //Add TextBox label
+                if (document.getElementsByClassName("dynamicTextFeild").length > 1){
+                    var removeTextFields = document.getElementsByClassName("dynamicTextFeild");
+                    for (obj of removeTextFields){
+                        obj.style.display = "block";
+                    }
+                }
+                else{
+                    var labelTf = document.createElement("label")
+                    labelTf.innerHTML = "Value*:";
+                    labelTf.className = "dynamicTextFeild";
+                    labelTf.setAttribute("for", "dynamicTF");
+
+                    var formDiv = document.getElementById("form_elements");
+                    var textFeild = document.createElement("input");
+                    textFeild.className = "dynamicTextFeild";
+                    textFeild.id = "dynamicTF";
+                    textFeild.name = "dynamicTF";
+                    textFeild.setAttribute("type", "text");
+                    
+                    formDiv.appendChild(labelTf);
+                    formDiv.appendChild(textFeild);
+                }
+                
+
+            } else {
+                var removeTextFields = document.getElementsByClassName("dynamicTextFeild");
+                for (obj of removeTextFields){
+                    obj.style.display = "none";
+                }
+            }
+        });
+        chkbox.style.float = "left";
+        formDiv.appendChild(chkbox);
+        var labl = document.createElement("label");
+        labl.name = "checkBoxLabel";
+        labl.innerHTML = selectCheckboxOptions[obj.selectedIndex];
+        chkbox.setAttribute("value", selectCheckboxOptions[obj.selectedIndex]);
+        formDiv.appendChild(labl);
+        labl.setAttribute("id", "labelTochange")
+        formDiv.appendChild(document.createElement("br"));
+        formDiv.appendChild(document.createElement("br")); 
+        
+    }
+}
+
+function addTextField(obj){
+    if (obj){
+        var formDiv = document.getElementById("form_elements");
+        var textFeild = document.createElement("input");
+        textFeild.setAttribute("type", "text");
+        formDiv.appendChild(textFeild);
+    }
+    
+}
